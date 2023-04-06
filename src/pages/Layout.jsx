bd48e81ef48249aecdc502a3eb602a06aff6d7b6
Tri Nguyen <nhokpoc102@gmail.com>
2023-04-06 18:19:29 +0700

    fix url

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Menu from "../components/Menu";
 import Sound from "react-sound";
 import { useEffect, useState } from "react";
-const bgSound = new URL("../assets/music/bg-sound.mp3", import.meta.url).href;
 
 export default function Layout() {
   const [control, setControl] = useState({
@@ -20,7 +19,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col h-screen gap-2 p-4 pb-12 max-w-[380px] w-full sm:max-w-full sm:w-[380px] mx-auto">
       <Sound
-        url={bgSound}
+        url={new URL("../assets/music/bg-sound.mp3", import.meta.url).href}
         playStatus={control.playStatusMusic}
         playFromPosition={0}
         volume={50}
