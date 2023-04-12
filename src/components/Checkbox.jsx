db0e057d8cda78c1d5a2ccc10931a651f3f68bad
Tri Nguyen <nhokpoc102@gmail.com>
2023-04-12 16:43:51 +0700

    add operation

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -3,7 +3,13 @@ import { useContext } from "react";
 import useSound from "use-sound";
 import { EffectContext } from "./EffectContext";
 
-export default function Checkbox(props) {
+export default function Checkbox({
+  children,
+  id,
+  checked,
+  values = [],
+  ...props
+}) {
   const [play] = useSound(tapSound);
   const { effect } = useContext(EffectContext);
 
@@ -11,9 +17,30 @@ export default function Checkbox(props) {
     if (props.onClick) props.onClick(ev);
     if (effect) play();
   };
-  return (
-    <input type="checkbox" {...props} onClick={handleClick}>
-      {props.children}
-    </input>
+
+  const handleChange = (ev) => {
+    if (props.onChange) props.onChange(ev);
+  };
+  return !children ? (
+    <input type="checkbox" {...props} onClick={handleClick} />
+  ) : (
+    <div className="relative w-max">
+      <input
+        className="peer hidden"
+        id={id}
+        name={id}
+        checked={checked}
+        onClick={handleClick}
+        onChange={handleChange}
+        type="checkbox"
+      />
+      <label
+        htmlFor={id}
+        {...props}
+        className="inline-flex items-center justify-between w-full px-4 py-2 border-2 border-gray-200 rounded-lg peer-checked:border-primary"
+      >
+        <span>{children}</span>
+      </label>
+    </div>
   );
 }
--- src/pages/CalculatePage.jsx
+++ src/pages/CalculatePage.jsx
@@ -1,7 +1,79 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
+import Checkbox from "../components/Checkbox";
 import Dialog from "../components/Dialog";
 import { CALCULATE } from "../helper/constants";
+
+const OPERATIONS = [
+  {
+    id: "plus",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-8 h-8"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+      </svg>
+    ),
+  },
+  {
+    id: "minus",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-8 h-8"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+      </svg>
+    ),
+  },
+  {
+    id: "multiply",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-8 h-8"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: "divide",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="w-8 h-8"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+        />
+      </svg>
+    ),
+  },
+];
 
 export default function CalculatePage() {
   const { COLS, ROWS, NUMBER_CAL, SCOPE, TIMEOUT } = CALCULATE;
@@ -9,11 +81,15 @@ export default function CalculatePage() {
     [...new Array(NUMBER_CAL)].fill(null)
   );
   const [finalResult, setFinalResult] = useState();
-  const [operation, setOperation] = useState(0);
+  const [operation, setOperation] = useState(["plus"]);
+  const [tempOperation, setTempOperation] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
+  const [openOperation, setOpenOperation] = useState(false);
   const [point, setPoint] = useState({ right: 0, wrong: 0 });
   const [notify, setNotify] = useState("");
   const [currentLevel, setCurrentLevel] = useState(1);
+  const [currentOperation, setCurrentOperation] = useState("plus");
+  const [isLogic, setIsLogic] = useState(false);
   const [arrNumber, setArrNumber] = useState([
     ...Array.from({ length: ROWS * COLS }, () => {
       return {
@@ -28,10 +104,7 @@ export default function CalculatePage() {
   };
 
   const strOperation = (value) => {
-    switch (value) {
-      case 0:
-        return "+";
-    }
+    return OPERATIONS.find((item) => item.id === value).svg;
   };
 
   useEffect(() => {
@@ -40,10 +113,8 @@ export default function CalculatePage() {
     );
 
     if (foundEmpty < 0) {
-      const answer = selected.reduce(
-        (prev, cur) => +prev + +arrNumber[cur]?.value,
-        0
-      );
+      const arrSelected = selected.map((item) => arrNumber[item].value);
+      const answer = finalResultOperation(arrSelected, currentOperation);
 
       setTimeout(() => {
         const strAnswer = answer === finalResult ? "right" : "wrong";
@@ -102,9 +173,31 @@ export default function CalculatePage() {
       const random = Math.floor(Math.random() * arrIsDisplay.length);
       arr.push(arrIsDisplay.splice(random, 1)[0].value);
     }
-    const finalResult = arr.reduce((prev, cur) => prev + cur, 0);
+    const random = operation[Math.floor(Math.random() * operation.length)];
+    const finalResult = finalResultOperation(arr, random);
+
+    setCurrentOperation(random);
     setFinalResult(finalResult);
     return finalResult;
+  };
+
+  const finalResultOperation = (arr, operation) => {
+    let data = 0;
+    switch (operation) {
+      case "plus":
+        data = arr.reduce((prev, cur) => prev + cur);
+        break;
+      case "minus":
+        data = arr.sort((a, b) => b - a).reduce((prev, cur) => prev - cur);
+        break;
+      case "multiply":
+        data = arr.reduce((prev, cur) => prev * cur);
+        break;
+      case "divide":
+        data = arr.reduce((prev, cur) => prev / cur);
+        break;
+    }
+    return data;
   };
 
   const resetSelected = async () => {
@@ -156,29 +249,81 @@ export default function CalculatePage() {
     setOpenDialog(false);
   };
 
+  const toggleLogic = () => {
+    setIsLogic((prev) => !prev);
+  };
+
+  const toggleOpenOperation = () => {
+    setOpenOperation((prev) => {
+      setTempOperation(!prev ? operation : []);
+      return !prev;
+    });
+  };
+
+  const changeOperation = () => {
+    // setTempOperation(operation);
+    setOperation((prev) => {
+      resetSelected();
+      resetArrNumber();
+      toggleOpenOperation();
+      return tempOperation;
+    });
+  };
+
+  const changeTempOperation = (ev) => {
+    const name = ev.target.name;
+    setTempOperation((prev) => {
+      const arr = prev.includes(name)
+        ? [...prev.filter((item) => item !== name)]
+        : [...prev, name];
+      return arr;
+    });
+  };
+
   return (
     <div className="flex flex-col justify-between h-full gap-2 relative">
       <div className="absolute w-full top-0 -mt-[52px] right-0">
         <div className="flex items-center justify-between">
           <p></p>
-          <h3 className="pl-10 text-phudu font-bold">Phép tính</h3>
-          <Button
-            onClick={toggleDialog}
-            className="bg-primary text-white p-1 rounded-full"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-7 h-7"
+          <h3 className="pl-20 text-phudu font-bold">Phép tính</h3>
+          <div className="flex gap-2">
+            <Button
+              onClick={toggleOpenOperation}
+              className="bg-tertiary text-white p-1 rounded-full"
             >
-              <path
-                fillRule="evenodd"
-                d="M12 5.25c1.213 0 2.415.046 3.605.135a3.256 3.256 0 013.01 3.01c.044.583.077 1.17.1 1.759L17.03 8.47a.75.75 0 10-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 00-1.06-1.06l-1.752 1.751c-.023-.65-.06-1.296-.108-1.939a4.756 4.756 0 00-4.392-4.392 49.422 49.422 0 00-7.436 0A4.756 4.756 0 003.89 8.282c-.017.224-.033.447-.046.672a.75.75 0 101.497.092c.013-.217.028-.434.044-.651a3.256 3.256 0 013.01-3.01c1.19-.09 2.392-.135 3.605-.135zm-6.97 6.22a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.752-1.751c.023.65.06 1.296.108 1.939a4.756 4.756 0 004.392 4.392 49.413 49.413 0 007.436 0 4.756 4.756 0 004.392-4.392c.017-.223.032-.447.046-.672a.75.75 0 00-1.497-.092c-.013.217-.028.434-.044.651a3.256 3.256 0 01-3.01 3.01 47.953 47.953 0 01-7.21 0 3.256 3.256 0 01-3.01-3.01 47.759 47.759 0 01-.1-1.759L6.97 15.53a.75.75 0 001.06-1.06l-3-3z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-7 h-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+                />
+              </svg>
+            </Button>
+            <Button
+              onClick={toggleDialog}
+              className="bg-primary text-white p-1 rounded-full"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-7 h-7"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 5.25c1.213 0 2.415.046 3.605.135a3.256 3.256 0 013.01 3.01c.044.583.077 1.17.1 1.759L17.03 8.47a.75.75 0 10-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 00-1.06-1.06l-1.752 1.751c-.023-.65-.06-1.296-.108-1.939a4.756 4.756 0 00-4.392-4.392 49.422 49.422 0 00-7.436 0A4.756 4.756 0 003.89 8.282c-.017.224-.033.447-.046.672a.75.75 0 101.497.092c.013-.217.028-.434.044-.651a3.256 3.256 0 013.01-3.01c1.19-.09 2.392-.135 3.605-.135zm-6.97 6.22a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.752-1.751c.023.65.06 1.296.108 1.939a4.756 4.756 0 004.392 4.392 49.413 49.413 0 007.436 0 4.756 4.756 0 004.392-4.392c.017-.223.032-.447.046-.672a.75.75 0 00-1.497-.092c-.013.217-.028.434-.044.651a3.256 3.256 0 01-3.01 3.01 47.953 47.953 0 01-7.21 0 3.256 3.256 0 01-3.01-3.01 47.759 47.759 0 01-.1-1.759L6.97 15.53a.75.75 0 001.06-1.06l-3-3z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Button>
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-2 text-center">
@@ -204,7 +349,26 @@ export default function CalculatePage() {
         </table>
       </div>
       <div className="flex flex-col flex-auto justify-center text-4xl font-semibold py-4 border-2 rounded-lg">
-        <div className="flex flex-1 items-center justify-center">
+        <div className="flex flex-col flex-1 items-center justify-center">
+          {/* <Button
+            onClick={toggleLogic}
+            className="flex text-lg bg-accent/10 rounded-full"
+          >
+            <p
+              className={`px-4 py-1 rounded-full ${
+                !isLogic ? "bg-black text-white" : ""
+              }`}
+            >
+              B
+            </p>
+            <p
+              className={`px-4 py-1 rounded-full ${
+                isLogic ? "bg-black text-white " : ""
+              }`}
+            >
+              L
+            </p>
+          </Button> */}
           <p
             className={`${
               !notify ? "text-black" : "text-white bg-secondary/70"
@@ -218,12 +382,27 @@ export default function CalculatePage() {
             <p className="text-black bg-accent/10 rounded-lg h-16 w-16 p-3">
               {arrNumber[selected[0]]?.value ?? ""}
             </p>
-            <p>{strOperation(operation)}</p>
+            <p>{strOperation(currentOperation)}</p>
             <p className="text-black bg-accent/10 rounded-lg h-16 w-16 p-3">
               {arrNumber[selected[1]]?.value ?? ""}
             </p>
-            <p>=</p>
-            <p className="text-black bg-accent/10 rounded-lg h-16 w-16 p-3">
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                />
+              </svg>
+            </p>
+            <p className="text-black bg-accent/10 rounded-lg h-16 min-w-16 p-3">
               {finalResult ?? ""}
             </p>
           </div>
@@ -264,6 +443,36 @@ export default function CalculatePage() {
           >
             Đóng
           </Button>
+        </div>
+      </Dialog>
+      <Dialog open={openOperation} classDialog={"!max-w-[280px]"}>
+        <div className="flex flex-col h-[240px] w-[200px] m-auto text-center justify-evenly text-phudu">
+          <div className="grid grid-cols-2 gap-4 w-max m-auto">
+            {OPERATIONS.map(({ id, svg }) => (
+              <Checkbox
+                key={id}
+                id={id}
+                checked={tempOperation.includes(id)}
+                onChange={changeTempOperation}
+              >
+                {svg}
+              </Checkbox>
+            ))}
+          </div>
+          <div className="flex gap-4">
+            <Button
+              onClick={changeOperation}
+              className="px-4 py-2 text-lg font-semibold w-full mx-auto rounded-lg bg-tertiary/80 text-white"
+            >
+              Đồng ý
+            </Button>
+            <Button
+              onClick={toggleOpenOperation}
+              className="px-4 py-2 text-lg font-semibold w-full mx-auto rounded-lg bg-quaternary/80 text-white"
+            >
+              Hủy
+            </Button>
+          </div>
         </div>
       </Dialog>
     </div>
